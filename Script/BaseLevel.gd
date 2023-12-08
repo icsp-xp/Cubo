e342bb09e4c0f00592af8938ca28c8cc107ccef6
CarlinoAlfredo <alfredocarlinogithub@gmail.com>
2023-12-08 15:18:38 +0100

    aggiunta la gestione dei segnali tramite la classe GestioneSegnali

--- Script/BaseLevel.gd
+++ Script/BaseLevel.gd
@@ -1,25 +1,12 @@
 extends Node2D # BaseLevel
 
-class_name BaseLevel
-
-@export_category("Connessione Segnali")
-# struttura dizionario
-#struttura_segnali
-#{
-#	"percorso_nodo_partenza" : 
-#			{
-#				percorso_nodo_arrivo : [[nome segnali da connettere, nome segnale da ricevere]]
-#			}
-#}
-@export var struttura_segnali : Dictionary = {}
+@onready var gestione_segnali = $GestioneSegnali as GestioneSegnali
 
 
 func _ready() -> void:
-	
-	for percorso_nodo_partenza in struttura_segnali.keys():
-		for percorso_nodo_arrivo in struttura_segnali.get(percorso_nodo_partenza):
-			var nodo_partenza : Node = get_node(percorso_nodo_partenza)
-			var nodo_arrivo : Node = get_node(percorso_nodo_arrivo)
-			
-			for coppia_segnali in struttura_segnali[percorso_nodo_partenza].get(percorso_nodo_arrivo):
-				nodo_partenza.connect(coppia_segnali[0], Callable(nodo_arrivo, coppia_segnali[1]))
+	gestione_segnali.aggiungi_segnale("../Player", "/root/Main/UI", "danneggiato", "_on_player_danneggiato")
+	gestione_segnali.aggiungi_segnale("../Player", "/root/Main/UI", "perso", "_on_player_perso")
+	gestione_segnali.aggiungi_segnale("../Player", "/root/Main/Camera2D", "danneggiato", "_on_player_danneggiato")
+	gestione_segnali.aggiungi_segnale("../Player", "/root/Main/Effetti", "danneggiato", "_on_player_danneggiato")
+	gestione_segnali.aggiungi_segnale("../Player", "/root/Main", "perso", "_on_player_perso")
+	gestione_segnali.connetti_segnali()

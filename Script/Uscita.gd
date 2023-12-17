ba721c1cf25746bd8b8debcc1eddb4a223aac5b4
CarlinoAlfredo <alfredocarlinogithub@gmail.com>
2023-12-17 19:26:05 +0100

    aggiunti altri effetti sonori e aggiunti altri tasti nel menu

--- Script/Uscita.gd
+++ Script/Uscita.gd
@@ -3,12 +3,14 @@ extends Marker2D # Uscita
 signal chiavi_prese()
 
 @onready var collision_shape_2d = $AreaUscita/CollisionShape2D as CollisionShape2D
+@onready var audio_prossimo_livello = $AudioProssimoLivello as AudioStreamPlayer
 
 var puo_uscire : bool = false
 
 
 func _on_player_presa_chiave() -> void:
 	if ScriptGlobale.numero_corrente_di_chiavi <= 0:
+		audio_prossimo_livello.play()
 		chiavi_prese.emit()
 		collision_shape_2d.call_deferred("set_disabled", false)
 
